148410fd46793bc0b862f04f6bf1635f3b80aad6
ATS88GR <turok.artyom@gmail.com>
2023-11-13 18:06:21 +0300

    add m3, t5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double division = ((double) toBeDivided)/5;
+        System.out.println(division);
     }
 }
